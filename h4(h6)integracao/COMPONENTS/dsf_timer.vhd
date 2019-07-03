4e9f9371aa75a9eda3e11a068cceb6fce6845e78
Caio Telles <cast@icomp.ufam.edu.br>
2019-07-03 11:21:53 -0400

    Correção do bloco do pulso

--- h4(h6)integracao/COMPONENTS/dsf_timer.vhd
+++ h4(h6)integracao/COMPONENTS/dsf_timer.vhd
@@ -5,28 +5,25 @@ entity dsf_timer is
 	port (
 			clk       : in std_logic;
 			trig   	 : in std_logic;
-			buz   	 : out std_logic
+			buz   	 : buffer std_logic
 	);
 end dsf_timer;
 
 architecture arch of dsf_timer is
-signal buz_output : std_logic := '0';
-
 begin
 	process(clk)
 	variable counter : integer range 0 to 1000 := 0;
 	begin
 		if rising_edge(clk) then
 			if trig = '1' then
-				buz_output <= '1';
+				buz <= '1';
 			end if;
-			if buz_output = '1' and counter < 1000 then
+			if buz = '1' and counter < 1000 then
 				counter := counter + 1;
-			elsif buz_output = '1' and counter >= 1000 then
+			elsif buz = '1' and counter >= 1000 then
 				counter := 0;
-				buz_output <= '0';
+				buz <= '0';
 			end if;
 		end if;
-	buz <= buz_output;
 	end process;
 end arch;
--- h4(h6)integracao/COMPONENTS/pulse_box.vhd
+++ h4(h6)integracao/COMPONENTS/pulse_box.vhd
@@ -1,21 +1,26 @@
-library ieee;
 use ieee.std_logic_1164.all;
 
 entity pulse_box is
 	port (
 			clk        : in std_logic;
 			eot   	  : in std_logic;
-			trig_pulse : buffer std_logic
+			trig_pulse 	  : buffer std_logic
 	);
 end pulse_box;
 
 architecture arch of pulse_box is
-begin	
+begin
 	process(clk,eot)
+	variable charged : boolean := false;
 	begin
-		if clk'event and clk = '0' and eot = '1' then 
+		if clk'event and clk = '0' and eot = '0' then
+			charged := false;
+		end if;
+
+		if clk'event and clk = '0' and eot = '1' and charged = false then
 			trig_pulse <= '1';
-		elsif clk'event and clk = '0' and trig_pulse = '1' then
+			charged := true;
+		elsif clk'event and clk = '0' and charged = true then
 			trig_pulse <= '0';
 		end if;
 	end process;
